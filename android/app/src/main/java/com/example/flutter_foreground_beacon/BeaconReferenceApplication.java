52089db32ea17ad6665cd24604b8e80ec64bac56
Brian Lin <ys.ncku@gmail.com>
2021-06-19 15:06:24 +0800

    Support foreground beacon scanning and advertising.

---
+++ android/app/src/main/java/com/example/flutter_foreground_beacon/BeaconReferenceApplication.java
@@ -0,0 +1,207 @@
+package com.example.flutter_foreground_beacon;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import android.os.Build;
+import android.util.Log;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.BeaconTransmitter;
+import org.altbeacon.beacon.Beacon;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseSettings;
+import java.util.Arrays;
+
+/**
+ * Created by dyoung on 12/13/13.
+ */
+public class BeaconReferenceApplication extends Application implements BootstrapNotifier {
+    private static final String TAG = "BeaconReferenceApp";
+    private RegionBootstrap regionBootstrap;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    private String cumulativeLog = "";
+
+    public void onCreate() {
+        super.onCreate();
+        BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
+
+        // By default the AndroidBeaconLibrary will only find AltBeacons.  If you wish to make it
+        // find a different type of beacon, you must specify the byte layout for that beacon's
+        // advertisement with a line like below.  The example shows how to find a beacon with the
+        // same byte layout as AltBeacon but with a beaconTypeCode of 0xaabb.  To find the proper
+        // layout expression for other beacon types, do a web search for "setBeaconLayout"
+        // including the quotes.
+        //
+        //beaconManager.getBeaconParsers().clear();
+        //beaconManager.getBeaconParsers().add(new BeaconParser().
+        //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        // The example shows how to find iBeacon.
+        beaconManager.getBeaconParsers().add(
+                new BeaconParser().
+                        setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+//        beaconManager.setDebug(true);
+
+        // Uncomment the code below to use a foreground service to scan for beacons. This unlocks
+        // the ability to continually scan for long periods of time in the background on Andorid 8+
+        // in exchange for showing an icon at the top of the screen and a always-on notification to
+        // communicate to users that your app is using resources in the background.
+        //
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.drawable.launch_background);
+        builder.setContentTitle("Scanning for Beacons");
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        builder.setContentIntent(pendingIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
+                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("My Notification Channel Description");
+            NotificationManager notificationManager = (NotificationManager) getSystemService(
+                    Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channel.getId());
+        }
+        beaconManager.enableForegroundServiceScanning(builder.build(), 456);
+
+        // For the above foreground scanning service to be useful, you need to disable
+        // JobScheduler-based scans (used on Android 8+) and set a fast background scan
+        // cycle that would otherwise be disallowed by the operating system.
+        //
+        beaconManager.setEnableScheduledScanJobs(false);
+        beaconManager.setBackgroundBetweenScanPeriod(0);
+        beaconManager.setBackgroundScanPeriod(1100);
+
+        Log.d(TAG, "setting up background monitoring for beacons and power saving");
+        // wake up the app when a beacon is seen
+        Region region = new Region("7BC06B1A-3D9E-4661-B89A-57774150F00D",
+                null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+
+        // simply constructing this class and holding a reference to it in your custom Application
+        // class will automatically cause the BeaconLibrary to save battery whenever the application
+        // is not visible.  This reduces bluetooth power usage by about 60%
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+
+        // If you wish to test beacon detection in the Android Emulator, you can use code like this:
+        // BeaconManager.setBeaconSimulator(new TimedBeaconSimulator() );
+        // ((TimedBeaconSimulator) BeaconManager.getBeaconSimulator()).createTimedSimulatedBeacons();
+
+        Beacon beacon = new Beacon.Builder()
+                .setId1("CB10023F-A318-3394-4199-A8730C7C1AEC")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x004c) // Radius Networks.  Change this for other beacon layouts
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[] {0l})) // Remove this for beacon layouts without d: fields
+                .build();
+
+        // Change the layout below for other beacon types
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e(TAG, "Advertisement start failed with code: "+errorCode);
+            }
+
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                Log.i(TAG, "Advertisement start succeeded.");
+            }
+        });
+    }
+
+    public void disableMonitoring() {
+        if (regionBootstrap != null) {
+            regionBootstrap.disable();
+            regionBootstrap = null;
+        }
+    }
+
+    public void enableMonitoring() {
+        Region region = new Region("backgroundRegion",
+                null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+    }
+
+    @Override
+    public void didEnterRegion(Region arg0) {
+        Log.d(TAG, "did enter region.");
+        // Send a notification to the user whenever a Beacon
+        // matching a Region (defined above) are first seen.
+        Log.d(TAG, "Sending notification.");
+        sendNotification();
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        logToDisplay("I no longer see a beacon.");
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int state, Region region) {
+        logToDisplay("Current region state is: " + (state == 1 ? "INSIDE" : "OUTSIDE ("+state+")"));
+    }
+
+    private void sendNotification() {
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Beacon Reference Notifications",
+                    "Beacon Reference Notifications", NotificationManager.IMPORTANCE_HIGH);
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            notificationManager.createNotificationChannel(channel);
+            builder = new Notification.Builder(this, channel.getId());
+        }
+        else {
+            builder = new Notification.Builder(this);
+            builder.setPriority(Notification.PRIORITY_HIGH);
+        }
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntent(new Intent(this, MainActivity.class));
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setSmallIcon(R.drawable.launch_background);
+        builder.setContentTitle("I detect a beacon");
+        builder.setContentText("Tap here to see details in the reference app");
+        builder.setContentIntent(resultPendingIntent);
+        notificationManager.notify(1, builder.build());
+    }
+
+    private void logToDisplay(String line) {
+        cumulativeLog += (line + "\n");
+        Log.i(TAG, cumulativeLog);
+    }
+
+    public String getLog() {
+        return cumulativeLog;
+    }
+
+}
--- android/app/src/main/java/com/example/flutter_foreground_beacon/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_foreground_beacon/MainActivity.java
@@ -1,6 +1,71 @@
 package com.example.flutter_foreground_beacon;
 
+import android.content.BroadcastReceiver;
+import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.EventChannel.EventSink;
+import io.flutter.plugin.common.EventChannel.StreamHandler;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.MethodCall;
 
 public class MainActivity extends FlutterActivity {
+  private static final String BATTERY_CHANNEL = "samples.flutter.io/battery";
+  private static final String CHARGING_CHANNEL = "samples.flutter.io/charging";
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    new EventChannel(flutterEngine.getDartExecutor(), CHARGING_CHANNEL).setStreamHandler(
+      new StreamHandler() {
+        private BroadcastReceiver chargingStateChangeReceiver;
+        EventSink eventSink = null;
+
+        @Override
+        public void onListen(Object arguments, EventSink events) {
+          eventSink = events;
+          new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              //Write whatever to want to do after delay specified (1 sec)
+              eventSink.success("charging");
+            }
+          }, 500);
+        }
+
+        @Override
+        public void onCancel(Object arguments) {
+        }
+      }
+    );
+
+    new MethodChannel(flutterEngine.getDartExecutor(), BATTERY_CHANNEL).setMethodCallHandler(
+      new MethodCallHandler() {
+        @Override
+        public void onMethodCall(MethodCall call, Result result) {
+          if (call.method.equals("getBatteryLevel")) {
+            String name = call.argument("name");
+            int age = call.argument("age");
+            Log.d("test", "name="+name);
+            int batteryLevel = 12;
+
+            if (batteryLevel != -1) {
+              result.success(batteryLevel);
+            } else {
+              result.error("UNAVAILABLE", "Battery level not available.", null);
+            }
+          } else {
+            result.notImplemented();
+          }
+        }
+      }
+    );
+  }
+
 }
